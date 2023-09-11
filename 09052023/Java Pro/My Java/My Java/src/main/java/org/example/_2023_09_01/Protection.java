113f29e25be9a0e03269b281ef7acbb198f08dc1
Lana <132999230+LanaZahorniuk@users.noreply.github.com>
2023-09-11 23:16:05 +0200

    Delete 09052023/Java Pro/My Java directory

--- 09052023/Java Pro/My Java/My Java/src/main/java/org/example/_2023_09_01/Protection.java
+++
@@ -1,6 +0,0 @@
-package org.example._2023_09_01;
-
-public class Protection {
-    String name;
-    int level;
-}
--- 09052023/Java Pro/My Java/My Java/src/main/java/org/example/_2023_09_04/ShippingCharge.java
+++
@@ -1,5 +0,0 @@
-package org.example._2023_09_04;
-
-public interface ShippingCharge {
-    void delivery (double charge);
-}
